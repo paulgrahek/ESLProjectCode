12da4dacaa9f551ba66eadae8dba72aad48c3346
Paul Grahek <paul.p.grahek@gmail.com>
2020-04-12 21:15:53 -0600

    updated prototype

--- eslproject/eslproject.ino
+++ eslproject/eslproject.ino
@@ -3,28 +3,66 @@
 
 int opticalSensorPin = A3;
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+const float v_in_max = 4;
+const float v_in_min = 1;
+const float v_on = 2;
+const float v_switch = 3;
+const float v_switch_rate = 12;
+
+float normalizeVoltage(float volt){
+  return (volt- v_in_min)/(v_in_max-v_in_min);
+}
+
+float v_switch_norm = normalizeVoltage(v_switch);
+
+float coeffs[16][4] = {
+  // Duty Range, Duty Offset, Switch Rate, Switch Offset
+  // LED_1 RGBA (SIDE)
+  {0.35, 0.26, v_switch_rate, v_switch_norm},
+  {0, 0.84, 0, 0},
+  {0.17, 0.1, -v_switch_rate, v_switch_norm},
+  {0.07, 0.87, v_switch_rate, v_switch_norm},
+  // LED_2 RGBA (CENTER)
+  {0.22, 0.26, v_switch_rate, v_switch_norm},
+  {0, 0.84, 0, 0},
+  {0.17, 0.13, -v_switch_rate, v_switch_norm},
+  {0.07, 0.8, v_switch_rate, v_switch_norm},
+  // LED_3 RGBA (CENTER)
+  {0.22, 0.26, v_switch_rate, v_switch_norm},
+  {0, 0.84, 0, 0},
+  {0.17, 0.13, -v_switch_rate, v_switch_norm},
+  {0.07, 0.8, v_switch_rate, v_switch_norm},
+  // LED_4 RGBA (SIDE)
+  {0.35, 0.26, v_switch_rate, v_switch_norm},
+  {0, 0.84, 0, 0},
+  {0.17, 0.1, -v_switch_rate, v_switch_norm},
+  {0.07, 0.87, v_switch_rate, v_switch_norm}
+};
+
 int offTimes[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 float sensorReadings[36] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 int sensorReadingsInd = 0;
-float sensorReading = 1; 
 
 void setup() {
     // Serial.begin(9600);
     pwm.begin();
-    pwm.setPWMFreq(500);
+    pwm.setPWMFreq(1000);
 }
 
 void loop() {
   // Drive each PWM in a 'wave'
-  getConditionedOpticalSensorReading();
-  getPWMDutyCycleOffTimes();
-  writePWMDutyCycles();
+  float sensorVoltageNormalized = getConditionedOpticalSensorReading();
+  writePWMDutyCycles(sensorVoltageNormalized);
   delay(10000);
 }
 
-// TODO: Account for imperfect "1" from on state, ensure values returned vary 1 to 0 with "off" and "on" states, respectively
+// Returns optical sensor reading as normalized voltage
 float getOpticalSensorReading(){
-  return 1.0-(float)analogRead(opticalSensorPin)/1023.0;
+  float rawReading = (float)analogRead(opticalSensorPin);
+  float normalizedReading = normalizeVoltage(rawReading);
+  if(normalizedReading < 0) return 0;
+  else if(normalizedReading > 1) return 1;
+  else return normalizedReading;
 }
 
 float getConditionedOpticalSensorReading(){
@@ -39,17 +77,15 @@ float getConditionedOpticalSensorReading(){
   for(int i = 0; i < 36; ++i){
     readingsSum += sensorReadings[36];
   }
-  return readingsSum/36.0;
+  return normalizeVoltage(readingsSum/36.0);
 }
 
-void getPWMDutyCycleOffTimes() {
+void writePWMDutyCycles(float sensorVoltageNormalized) {
   for(int i = 0; i < 16; ++i){
-    offTimes[i] = 1.0/(1.0+exp(-sensorReading))*4095.0;
-  }
-}
-
-void writePWMDutyCycles(){
-  for(int i = 0; i < 16; ++i){
-    pwm.setPWM(i,0,offTimes[i]);
+    float offTime = 0;
+    if (sensorVoltageNormalized > v_switch_norm){
+      offTime = (coeffs[i][0]*1.0/(1.0+exp(-coeffs[i][2]*(sensorVoltageNormalized-coeffs[i][3]))) + coeffs[i][1])*4095.0;  
+    }
+    pwm.setPWM(i,0,offTime);
   }
 }
